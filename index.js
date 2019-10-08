1bc576ca178927ce92f8279a9a91ab3bbd520d91
quanghuy1242 <katygaga124@gmail.com>
2019-10-08 20:41:26 +0700

    demo in js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+import { html, render } from 'lit-html';
+
 import './src/Components/Tooltip/qh-tooltip';
 import './src/Components/CircularProgress/qh-circular-progress';
+
+render(
+  html`
+    <style>
+      * {
+        margin: 0 auto;
+      }
+
+      body {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+      }
+
+      qh-circular-progress {
+        --mdc-theme-primary: #416275;
+      }
+
+    </style>
+    <qh-tooltip content="Đẹp trai">
+      <button id="btn-tooltip">Bạn Huy có một đặc điểm là rất</button>
+    </qh-tooltip>
+    <qh-circular-progress size="xlarge" center></qh-circular-progress>
+  `,
+  document.body
+)
