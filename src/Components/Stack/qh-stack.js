79d078049aca048d37f90d6d972664e5401d617a
Quang Huy <quanghuy1242@gmail.com>
2019-11-05 18:00:57 +0700

    new component <qh-stack></qh-stack>

---
+++ src/Components/Stack/qh-stack.js
@@ -0,0 +1,41 @@
+import { LitElement, customElement, html, css, property, unsafeCSS } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+
+@customElement('qh-stack')
+export class Stack extends LitElement {
+  @property({ type: Boolean }) horizontal = false;
+  @property({ type: Number }) gap = 0;
+
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        flex-direction: column;
+      }
+
+      :host([horizontal]) {
+        flex-direction: row;
+      }
+    `;
+  }
+
+  firstUpdated() {
+
+  }
+
+  render() {
+    return html`
+      <style>
+        :host > ::slotted(div:not(:last-child)) {
+          margin-bottom: ${unsafeCSS(this.gap)}px;
+        }
+
+        :host([horizontal]) > ::slotted(div:not(:last-child)) {
+          margin-bottom: inherit;
+          margin-right: ${unsafeCSS(this.gap)}px;
+        }
+      </style>
+      <slot></slot>
+    `;
+  }
+}
--- src/Pages/app-demo.js
+++ src/Pages/app-demo.js
@@ -2,6 +2,7 @@ import { LitElement, customElement, html, css } from 'lit-element';
 
 import '../Components/Tooltip/qh-tooltip';
 import '../Components/CircularProgress/qh-circular-progress';
+import '../Components/Stack/qh-stack';
 
 @customElement('app-demo')
 export class AppDemo extends LitElement {
@@ -12,12 +13,17 @@ export class AppDemo extends LitElement {
       }
     `;
   }
+
   render() {
     return html`
       <qh-tooltip content="Đẹp trai">
         <button id="btn-tooltip">Bạn Huy có một đặc điểm là rất</button>
       </qh-tooltip>
       <qh-circular-progress size="xlarge" center></qh-circular-progress>
+      <qh-stack horizontal gap=${16}>
+        <div>aaaaaaaaaaaaaaa</div>
+        <div>bbbbbbbbbbbbbbb</div>
+      </qh-stack>
     `;
   }
 }
